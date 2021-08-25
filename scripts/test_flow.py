f0bc36beecc92795c15970f93ee1b687a2c7db5f
DavidWuthier <dwuthier@gmail.com>
2021-08-25 16:13:54 -0400

    Added code for Flow Deck

---
+++ scripts/test_flow.py
@@ -0,0 +1,43 @@
+"""Takeoff-hover-land for one CF. Useful to validate hardware config."""
+
+from pycrazyswarm import Crazyswarm
+import numpy as np
+
+
+TAKEOFF_HEIGHT      = 0.5
+TAKEOFF_DURATION    = 2
+HOVER_DURATION      = 2
+MOVE_DURATION       = 2
+LAND_HEIGHT         = 0.05
+LAND_DURATION       = 2
+
+
+swarm = Crazyswarm()
+timeHelper = swarm.timeHelper
+cfs = swarm.allcfs.crazyflies
+
+
+def takeoff():
+    for cf in cfs:
+        cf.takeoff(targetHeight=TAKEOFF_HEIGHT, duration=TAKEOFF_DURATION)
+        timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)        
+
+def land():
+    for cf in cfs:
+        cf.land(targetHeight=LAND_HEIGHT, duration=LAND_DURATION)
+        timeHelper.sleep(LAND_DURATION)
+
+def move(pos):
+    for cf in cfs:
+        cf.goTo(goal=np.asarray(pos), yaw=0, duration=MOVE_DURATION)
+        timeHelper.sleep(MOVE_DURATION + HOVER_DURATION)
+
+def main():
+    print("initialPosition:",cfs[0].initialPosition)
+    takeoff()
+    move([1, 0, 0.5])
+    move([1, 1, 0.5])
+    land()
+
+if __name__ == "__main__":
+    main()
